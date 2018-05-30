011494d3e9242312a40878886c00193067b10d2c
Tushar Naik <tushar.knaik@gmail.com>
2018-05-30 12:17:54 +0530

    renaming a method

--- teflon-framework/src/main/java/com/teflon/task/framework/core/NonResumableSource.java
+++ teflon-framework/src/main/java/com/teflon/task/framework/core/NonResumableSource.java
@@ -6,7 +6,7 @@ import com.teflon.task.framework.core.meta.TaskStat;
  * @author tushar.naik
  * @version 1.0  30/05/18 - 10:14 AM
  */
-public interface NoResumeSource<Input, Progress> extends Source<Input, Progress> {
+public interface NonResumableSource<Input> extends Source<Input, Void> {
 
     default void resume(Task init, TaskStat taskStat) {
         throw new UnsupportedOperationException("Task " + init.name() + "cannot be resumed");
